dfb62b17422b65b10a09db4ebd6bddece2190c8e
mnzolo <mnzolo@student.wethinkcode.co.za>
2019-11-03 16:39:46 +0200

    upload image

---
+++ uploadimage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CAMAGRU</title>
+        <link rel="stylesheet" href="css/login.css">
+        <h1 align="center" >Image_Maker_</h1>
+        <hr>
+    </head>
+    <body style="background-color:white;">
+    <form method="post" enctype="multipart/form-data" action="uploads.php">
+        <div>
+            <input style="background-color: green" type="file" name="takeimage" value="uploadimage">
+        </div>
+        <div>
+            <input style="background-color: green" type="submit" name="takeimage"   value="uplaodimage">
+        </div>
+    </form>
+        <!-- <p>Uploads_Gallery_</p>
+        <div>
+        <div>!-->
+    </body>
+</html>
