d24e2e0ea5bdf1f3dd49ebd24819bf481c815a8f
mnzolo <mnzolo@student.wethinkcode.co.za>
2019-11-10 17:18:44 +0200

    home2

---
+++ camera.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CAMAGRU</title>
+        <link rel="stylesheet" href="css/login.css">
+        <h1 align="center" >Camera_</h1>
+        <hr>
+    </head>
+    <body style="background-color: white">
+    <div align="center" style="background-color: gray;width: 500px;height: 500px;border: 7px black solid; border-radius: 5px;">
+        <video id="videocam" autoplay="true">
+        </video>
+    <div>
+    </body>
+</html>
+
+<script>
+    var video = document.querySelector("#videocam");
+</script>
